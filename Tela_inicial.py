a759a5313953a7c1f9856f06f47f3387d991828c
Isabella <isabellasa@al.insper.edu.br>
2021-11-17 17:37:32 -0300

    dicionário de imagens

--- Tela_inicial.py
+++ Tela_inicial.py
@@ -9,7 +9,11 @@ altura = 600
 window = pygame.display.set_mode((altura, largura)) 
 pygame.display.set_caption('Flappy Fox')
 
-background = pygame.image.load('flappy_fox/img/p_fundo.png').convert()
+imagens = {}
+imagens['background'] = pygame.image.load('flappy_fox/img/p_fundo.png').convert()
+imagens['cano'] = pygame.image.load('flappy_fox/img/caninho.png').convert_alpha()
+imagens['raposa f'] = pygame.image.load('flappy_fox/img/raposa1.jpeg').convert_alpha()
+imagens['raposa a'] = pygame.image.load('flappy_fox/img/raposa2.jpeg').convert_alpha()
 
 game = True
 while game:
@@ -21,7 +25,8 @@ while game:
 
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(background, (0, 0))
+    window.blit(imagens['background'], (0, 0))
+
 
     pygame.display.update()
 
