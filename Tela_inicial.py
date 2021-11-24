762d31ff2aca503289b303feb0f1be5968d04576
Alexia <81188486+alexiabptech@users.noreply.github.com>
2021-11-24 17:19:20 -0300

    Implementando a animação da Raposa

--- Tela_inicial.py
+++ Tela_inicial.py
@@ -3,17 +3,26 @@ import pygame
 #Inicialização
 pygame.init()
 
-
-largura = 480
-altura = 600
-window = pygame.display.set_mode((altura, largura)) 
-pygame.display.set_caption('Flappy Fox')
+#Gera Tela Principal
+raposa_lar = 60
+raposa_alt = 40
+largura = 600
+altura = 500
+window = pygame.display.set_mode((largura,altura)) 
+pygame.display.set_caption('JumpFox')
 
 imagens = {}
 imagens['background'] = pygame.image.load('flappy_fox/img/p_fundo.png').convert()
 imagens['cano'] = pygame.image.load('flappy_fox/img/caninho.png').convert_alpha()
-imagens['raposa f'] = pygame.image.load('flappy_fox/img/raposa1.jpeg').convert_alpha()
-imagens['raposa a'] = pygame.image.load('flappy_fox/img/raposa2.jpeg').convert_alpha()
+
+#Animação da Raposa
+raposas = []
+for i in range(1,3):
+    filename = 'raposa_text.py/flappy_fox/img/raposa{}.png'.format(i)
+    img = pygame.image.load(filename).convert_alpha()
+    img = pygame.transform.scale(img,(raposa_lar,raposa_alt))
+    raposas.append(img)
+imagens["raposas"] = raposas
 
 game = True
 while game:
