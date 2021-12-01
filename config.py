a106ec9203d8bd9fa5eba1dec2e0b9926f43a2d4
Isabella <isabellasa@al.insper.edu.br>
2021-12-01 19:33:59 -0300

    config

---
+++ config.py
@@ -0,0 +1,24 @@
+# Estados para controle do fluxo da aplicação
+INIT = 0 #tela principal
+GAME = 1
+QUIT = 2 
+OVER = 3
+
+# Define algumas variáveis com as cores básicas
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+FPS = 10 
+# ----- Gera tela principal
+largura = 500
+altura = 500
+raposa_lar = 60
+raposa_alt = 40
+obst_lar = 40
+obst_alt = 30
+cerveja_lar = 20
+cerveja_alt = 30 
